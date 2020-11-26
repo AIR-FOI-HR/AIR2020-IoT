d6df49bfd70be6c20aa88c41b6d39b91c0ba2d46
avresk <vreskanja@gmail.com>
2020-11-26 22:02:07 +0100

    Commit obuhvaća dva commit-a ispod.

---
+++ LSM303C/run.py
@@ -0,0 +1,16 @@
+from main import app, db
+from app.LSMmain import Main
+from service import MqttClient
+
+
+def initialize():
+    print("initializing")
+    mqtt = MqttClient.Mqtt(MqttClient._topic)
+    main = Main(mqtt)
+    main.start()
+
+
+
+if __name__ == '__main__':
+    initialize()
+    app.run(debug=True, host='0.0.0.0', port=8080)
