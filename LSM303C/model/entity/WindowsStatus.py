b03c0f64a979a623a7295bf1698305b31e5afd11
avresk <vreskanja@gmail.com>
2020-11-26 22:23:57 +0100

    Kreiranje tablice windows_status te priprema DTO-a za isti.

---
+++ LSM303C/model/entity/WindowsStatus.py
@@ -0,0 +1,35 @@
+from main import db
+from datetime import datetime as dt
+from enum import Enum
+import math
+
+class Status(Enum):
+    OTVOREN = 1
+    ZATVOREN = 2
+    KIPER = 3
+
+
+
+class WindowsStatus(db.Model):
+    __tablename__ = 'windows_status'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    x = db.Column(db.Integer())
+    y = db.Column(db.Integer())
+    z = db.Column(db.Integer())
+    vector = db.Column(db.Integer())
+    status = db.Column(db.Integer(), unique=True)
+
+    @staticmethod
+    def create(x, y, z, s):
+        wStatus = WindowsStatus()
+        wStatus.x = x
+        wStatus.y = y
+        wStatus.z = z
+        wStatus.vector = int(math.sqrt(math.pow((x + y + z), 2)))
+        wStatus.status = s
+        return wStatus
+
+
+    def __repr__(self):
+        return str(self.__dict__)
